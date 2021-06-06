3d09098701471e46dd301c401ed3ffe9e79a6ded
kritika-r <82035781+kritika-r@users.noreply.github.com>
2021-06-06 18:27:19 +0530

    KwitterSocial@part-2

--- login.js
+++ login.js
@@ -1,5 +1,25 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCvyang3LGo4Ytle3G_fTjp7HC_mZtmBjE",
+    authDomain: "kwitterproject-f337a.firebaseapp.com",
+    databaseURL: "https://kwitterproject-f337a-default-rtdb.firebaseio.com",
+    projectId: "kwitterproject-f337a",
+    storageBucket: "kwitterproject-f337a.appspot.com",
+    messagingSenderId: "1059384883246",
+    appId: "1:1059384883246:web:fee231417fac0fe49407bf"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 function addUser(){
-    var username = document.getElementById("input").value;
-    localStorage.setItem("Username", username);
-    window.location = "kwitter.html";
+   
+    var username = document.getElementById("username_input").value;
+   firebase.database().ref("/").child(username).update({
+      textmessage : "adding username"
+   });
+   localStorage.setItem("username", username);
+  
+   
+}
+function next(e){
+   window.location = "kwitter.html";
 }
